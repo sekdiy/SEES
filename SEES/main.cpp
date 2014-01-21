b00a60a3ad4942cfb1f18e7d020a1f9b828e9ba1
sekdiy <sekdiy@googlemail.com>
2014-01-21 14:13:38 +0100

    Initial Commit

---
+++ SEES/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SEES
+//
+//  Created by Sebastian Kraus on 21/01/14.
+//  Copyright (c) 2014 Sebastian Kraus. All rights reserved.
+//
+
+#include "systemc.h"
+
+int sc_main (int argc, char * argv[]) {
+			
+    // insert code here...
+    std::cout << "Hello, SystemC!\n";
+    sc_start();
+    return 0;
+}
+
