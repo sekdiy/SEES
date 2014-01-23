bbe0bf613b0e54dd95748034cadb955361568850
sekdiy <sekdiy@googlemail.com>
2014-01-23 12:57:38 +0100

    Merged with Kim’s branch

--- SEES/main.cpp
+++ SEES/main.cpp
@@ -1,18 +1,26 @@
-//
-//  main.cpp
-//  SEES
-//
-//  Created by Sebastian Kraus on 21/01/14.
-//  Copyright (c) 2014 Sebastian Kraus. All rights reserved.
-//
-
-#include "systemc.h"
+#include "Passenger.h"
+#include "FloorBtnUp.h"
+#include <systemc>
+using namespace sc_core;
 
 int sc_main (int argc, char * argv[]) {
-			
-    // insert code here...
-    std::cout << "Hello, SystemC :)\n";
-    sc_start();
+
+    // Signals
+    sc_fifo<int> floorButtonUpSignal(1);
+
+    // Passengers
+    Passenger pass1("Peter");
+    //Passenger pass2("Paul");
+    //Passenger pass3("Mary");
+
+    // FloorButtons
+    FloorButtonUp fbu("ButtonUp");
+    fbu.requestUpFloor(floorButtonUpSignal);
+    pass1.requestUpFloor(floorButtonUpSignal);
+    //pass2.requestUpFloor(floorButtonUpSignal);
+    //pass3.requestUpFloor(floorButtonUpSignal);
+
+    sc_start(7, SC_SEC);
     return 0;
 }
 
